724d5bc7e0ff9410309af309e39cd7e07a610654
ArrayH <haoyr0805@126.com>
2019-10-12 15:18:16 +0800

    extract github config from code to properties

---
+++ src/main/java/com/array/community/community/provider/GithubProvider.java
@@ -0,0 +1,49 @@
+package com.array.community.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.array.community.community.dto.AccessTokenDTO;
+import com.array.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//@Component 只是把当前类初始化到spring容器的上下文，也就是不需要通过new一个变量去调用了
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+       MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(string);
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String str = response.body().string();
+            GithubUser githubUser = JSON.parseObject(str, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+            return null;
+    }
+}

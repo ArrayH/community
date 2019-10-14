ad2a1412f6e67137dd88447037f4593ad954f211
ArrayH <haoyr0805@126.com>
2019-10-14 17:32:12 +0800

    add check cookie

--- src/main/java/com/array/community/community/controller/AuthorizeController.java
+++ src/main/java/com/array/community/community/controller/AuthorizeController.java
@@ -11,58 +11,54 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
-    
+
     @Autowired
     private GithubProvider githubProvider;
 
     @Value("${github.client.id}")
-    private String ClientId;
+    private String clientId;
 
     @Value("${github.client.secret}")
-    private String ClientSecret;
+    private String clientSecret;
 
     @Value("${github.redirect.uri}")
-    private String RedirectUri;
+    private String redirectUri;
 
     @Autowired
     private UserMapper userMapper;
 
-
-
-
-    
     @GetMapping("/callback")
-    //@RequestParam 接收参数
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id(ClientId);
-        accessTokenDTO.setClient_secret(ClientSecret);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri(RedirectUri);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println("----------------------------------");
-        if(githubUser!=null) {
+        if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
-            user.setAccountId(String.valueOf(githubUser.getId()));
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登录成功,写cookie和session
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
-        }else {
-            //登录失败
+        } else {
+            // 登录失败，重新登录
             return "redirect:/";
         }
     }
--- src/main/java/com/array/community/community/mapper/UserMapper.java
+++ src/main/java/com/array/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.array.community.community.mapper;
 import com.array.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -12,4 +14,6 @@ public interface UserMapper {
             "#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
